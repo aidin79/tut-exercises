26188d2bad09e3a0a0cb42de91513b5cc469eebd
Aidin <aidinkiany@gmail.com>
2022-01-24 07:33:42 +0330

    image operations

---
+++ 4_image_operations.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as np
+
+image = cv.imread('test.jpeg')
+
+flower = image[80:380, 280:580]
+
+# print(cv.IMREAD_COLOR)
+cv.imshow('image', image)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+cv.imshow('flower', flower)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+image[80:380, 680:980] = flower
+
+cv.imshow('image', image)
+cv.waitKey(0)
+cv.destroyAllWindows()
